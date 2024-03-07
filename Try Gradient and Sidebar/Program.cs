b320c0febd3673e1a4df019656ffa6877813158f
bayudamarbr <bayudamar802@students.unnes.ac.id>
2024-03-07 11:05:51 +0700

    initial commit

---
+++ Try Gradient and Sidebar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Try_Gradient_and_Sidebar
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
